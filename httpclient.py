2dbf2c21cfd9ffa03bc4103c104eba630278d358
Marcos Del Sol Vives <socram8888@gmail.com>
2016-09-21 22:59:14 +0200

    Use requests for HTTP

--- httpclient.py
+++ httpclient.py
@@ -1,44 +1,29 @@
 # -*- coding: utf-8 -*-
 
-import urllib.request
-import urllib.parse
+import requests
 import json
 from lxml import etree
-
-def filter_null_values(orig):
-	new = dict()
-
-	for k in orig:
-		v = orig[k]
-		if v != None:
-			new[k] = v
-
-	return new
 
 class HttpClient:
 	def __init__(self):
 		self.timeout = 5
 		self.userAgent = None;
 
-	def get(self, url, params = {}, **kwargs):
-		params = filter_null_values(params)
-		if params != {}:
-			url = '%s?%s' % (url, urllib.parse.urlencode(params))
+	def request(self, url, params = {}, **kwargs):
+		if 'timeout' not in kwargs:
+			kwargs['timeout'] = self.timeout
+		if 'headers' not in kwargs:
+			kwargs['headers'] = {}
 
-		timeout = self.timeout
-		if 'timeout' in kwargs:
-			timeout = kwargs['timeout']
-
-		headers = filter_null_values({
+		kwargs['headers'].update({
 			'User-Agent': self.userAgent
 		})
 
-		request = urllib.request.Request(url, headers = headers)
-		response = urllib.request.urlopen(request, timeout = timeout)
-		return response.read()
+		return requests.get(url, params, **kwargs)
 
 	def getJSON(self, *args, **kwargs):
-		return json.loads(self.get(*args, **kwargs).decode('utf-8'))
+		return self.request(*args, **kwargs).json()
 
 	def getXML(self, *args, **kwargs):
-		return etree.fromstring(self.get(*args, **kwargs))
+		# TODO: figure out how to use stream
+		return etree.XML(self.request(*args, **kwargs).content)
