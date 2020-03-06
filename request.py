edee15e3fb8aa258eb6211a1cbcacb5930d9a69c
Marcos Del Sol Vives <marcos@orca.pet>
2020-03-06 12:06:22 +0100

    Use custom exception for request processing

--- request.py
+++ request.py
@@ -4,26 +4,29 @@ import re
 
 CMD_REGEX = re.compile(r'\/([a-z0-9]+)(?:@([a-z0-9_]+))?(?:\s+(.*))?', re.IGNORECASE)
 
+class InvalidRequestException(Exception):
+	pass
+
 class Request:
 	def __init__(self, bot, update):
 		self.bot = bot
 
 		if not 'text' in update['message']:
-                        raise ValueError("No message in update")
+                        raise InvalidRequestException("No message in update")
 
 		matches = re.match(CMD_REGEX, update['message']['text'])
 		if not matches:
-			raise ValueError("Not a command")
+			raise InvalidRequestException("Not a command")
 
 		botname = matches.group(2)
 		if botname and botname.casefold() != bot.name.casefold():
-			raise ValueError("Not for this bot")
+			raise InvalidRequestException("Not for this bot")
 
 		try:
 			self.commandName = matches.group(1).casefold()
 			self.command = bot.commands[self.commandName]
 		except KeyError:
-			raise ValueError("Command not found")
+			raise InvalidRequestException("Command not found")
 
 		self.params = matches.group(3)
 		if not self.params:
